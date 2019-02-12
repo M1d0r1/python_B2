2ff2f9cea682c72546370de6269393aa2c704b25
M1d0r1 <ultragreen@gmail.com>
2019-02-12 22:29:20 +0300

    Contact class and Address class were added

---
+++ contact.py
@@ -0,0 +1,21 @@
+class Contact:
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, primary_address, secondary_address, mobile_phone, work_phone, fax, email1, email2, email3, birthday, anniversary, notes)
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.primary_address = primary_address
+        self.secondary_address = secondary_address
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.birthday = birthday
+        self.anniversary = anniversary
+        self.notes = notes
+
+class Adress:
+    def __init__(self, geo_address, home_phone):
+        self.geo_address = geo_address
+        self.home_phone = home_phone
