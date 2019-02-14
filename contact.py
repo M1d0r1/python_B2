bfe13ac79092b2ca38eb9b86b6eba3dfb4801850
M1d0r1 <ultragreen@gmail.com>
2019-02-14 09:18:29 +0300

    Primary_address and secondary_address are now filled in.

--- contact.py
+++ contact.py
@@ -1,13 +1,13 @@
 class Contact:
-    def __init__(self, first_name, middle_name, last_name, nickname, title, company, mobile_phone, work_phone, fax, email1, email2, email3, notes):
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, primary_address, secondary_address, mobile_phone, work_phone, fax, email1, email2, email3, notes):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
         self.nickname = nickname
         self.title = title
         self.company = company
-        # self.primary_address = primary_address
-        # self.secondary_address = secondary_address
+        self.primary_address = primary_address
+        self.secondary_address = secondary_address
         self.mobile_phone = mobile_phone
         self.work_phone = work_phone
         self.fax = fax
