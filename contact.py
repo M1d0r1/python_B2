ad36d7ec7b94dd0b7aa4f1170caecb95c6f767bb
M1d0r1 <ultragreen@gmail.com>
2019-02-15 22:37:52 +0300

    Test classes for groups and contacts are now separated into two different modules. Address class was deleted: address and home phone are separate string fields.

--- contact.py
+++ contact.py
@@ -1,5 +1,5 @@
 class Contact:
-    def __init__(self, first_name, middle_name, last_name, nickname, title, company, primary_address, secondary_address, mobile_phone, work_phone, fax, email1, email2, email3, homepage, birthdate, anniversary_date, group,  notes):
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, primary_address, primary_home_phone, secondary_address, secondary_home_phone, mobile_phone, work_phone, fax, email1, email2, email3, homepage, birthdate, anniversary_date, group,  notes):
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
@@ -7,7 +7,9 @@ class Contact:
         self.title = title
         self.company = company
         self.primary_address = primary_address
+        self.primary_home_phone = primary_home_phone
         self.secondary_address = secondary_address
+        self.secondary_home_phone = secondary_home_phone
         self.mobile_phone = mobile_phone
         self.work_phone = work_phone
         self.fax = fax
@@ -20,7 +22,3 @@ class Contact:
         self.group = group
         self.notes = notes
 
-class Address:
-    def __init__(self, geo_address, home_phone):
-        self.geo_address = geo_address
-        self.home_phone = home_phone
