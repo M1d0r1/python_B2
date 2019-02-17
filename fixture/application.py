8e4b0d8b23af1799680c8fa28e9328f1241cc480
M1d0r1 <ultragreen@gmail.com>
2019-02-17 22:31:44 +0300

    Work with contacts moved to ContactHelper class

--- fixture/application.py
+++ fixture/application.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
-import os
+from fixture.contact import ContactHelper
+
 
 class Application:
     def __init__(self):
@@ -10,89 +10,13 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
-
-    def create_contact(self, contact):
-        # Init contact creation
-        self.navigate_to_contacts()
-        # Fill in the form
-        self.wd.find_element_by_name("firstname").click()
-        self.wd.find_element_by_name("firstname").clear()
-        self.wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        self.wd.find_element_by_name("middlename").click()
-        self.wd.find_element_by_name("middlename").clear()
-        self.wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        self.wd.find_element_by_name("lastname").click()
-        self.wd.find_element_by_name("lastname").clear()
-        self.wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        self.wd.find_element_by_name("nickname").click()
-        self.wd.find_element_by_name("nickname").clear()
-        self.wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        self.wd.find_element_by_name("photo").send_keys(os.getcwd() + r'\Resource\photo.jpg')
-        self.wd.find_element_by_name("title").click()
-        self.wd.find_element_by_name("title").clear()
-        self.wd.find_element_by_name("title").send_keys(contact.title)
-        self.wd.find_element_by_name("company").click()
-        self.wd.find_element_by_name("company").clear()
-        self.wd.find_element_by_name("company").send_keys(contact.company)
-        self.wd.find_element_by_name("address").click()
-        self.wd.find_element_by_name("address").clear()
-        self.wd.find_element_by_name("address").send_keys(contact.primary_address)
-        self.wd.find_element_by_name("home").click()
-        self.wd.find_element_by_name("home").clear()
-        self.wd.find_element_by_name("home").send_keys(contact.primary_home_phone)
-        self.wd.find_element_by_name("mobile").click()
-        self.wd.find_element_by_name("mobile").clear()
-        self.wd.find_element_by_name("mobile").send_keys(contact.mobile_phone)
-        self.wd.find_element_by_name("work").click()
-        self.wd.find_element_by_name("work").clear()
-        self.wd.find_element_by_name("work").send_keys(contact.work_phone)
-        self.wd.find_element_by_name("fax").click()
-        self.wd.find_element_by_name("fax").clear()
-        self.wd.find_element_by_name("fax").send_keys(contact.fax)
-        self.wd.find_element_by_name("email").click()
-        self.wd.find_element_by_name("email").clear()
-        self.wd.find_element_by_name("email").send_keys(contact.email1)
-        self.wd.find_element_by_name("email2").click()
-        self.wd.find_element_by_name("email2").clear()
-        self.wd.find_element_by_name("email2").send_keys(contact.email2)
-        self.wd.find_element_by_name("email3").click()
-        self.wd.find_element_by_name("email3").clear()
-        self.wd.find_element_by_name("email3").send_keys(contact.email3)
-        self.wd.find_element_by_name("homepage").click()
-        self.wd.find_element_by_name("homepage").clear()
-        self.wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        Select(self.wd.find_element_by_name("bday")).select_by_visible_text(str(contact.birthdate.day))
-        Select(self.wd.find_element_by_name("bmonth")).select_by_index(contact.birthdate.month)
-        self.wd.find_element_by_name("byear").clear()
-        self.wd.find_element_by_name("byear").send_keys(contact.birthdate.year)
-        Select(self.wd.find_element_by_name("aday")).select_by_visible_text(str(contact.anniversary_date.day))
-        Select(self.wd.find_element_by_name("amonth")).select_by_index(contact.anniversary_date.month)
-        self.wd.find_element_by_name("ayear").clear()
-        self.wd.find_element_by_name("ayear").send_keys(contact.anniversary_date.year)
-        self.wd.find_element_by_name("address2").click()
-        self.wd.find_element_by_name("address2").clear()
-        self.wd.find_element_by_name("address2").send_keys(contact.secondary_address)
-        self.wd.find_element_by_name("phone2").click()
-        self.wd.find_element_by_name("phone2").clear()
-        self.wd.find_element_by_name("phone2").send_keys(contact.secondary_home_phone)
-        Select(self.wd.find_element_by_name("new_group")).select_by_visible_text(contact.group.name)
-        self.wd.find_element_by_name("notes").click()
-        self.wd.find_element_by_name("notes").clear()
-        self.wd.find_element_by_name("notes").send_keys(contact.notes)
-        # Submit contact creation
-        self.wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
-        self.open_start_page()
-
+        self.contact = ContactHelper(self)
 
     def open_start_page(self):
         self.wd.get("http://localhost/addressbook/")
 
-
     def navigate_to_groups(self):
         self.wd.find_element_by_link_text("groups").click()
-
-    def navigate_to_contacts(self):
-        self.wd.find_element_by_link_text("add new").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,7 +13,7 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     # Set the index in order to distinguish the created groups and contacts
     tmstp = str(time.time())
     ind = tmstp[13:15]
@@ -22,10 +22,10 @@ def test_add_contact(app):
     anniversary_date = datetime.date(2005, 3, 14)
     # Create a group for the contact
     group = Group(name="Group" + ind, header="Header" + ind,footer="Footer" + ind)
-    app.create_group(group)
+    app.group.create(group)
     # Create the contact itself
     contact = Contact(first_name="Name"+ind, middle_name="Middle name"+ind, last_name="Last name"+ind, nickname="Nickname"+ind, title = "Title"+ind, company = "Company"+ind, primary_address="Country\nCity\nStreet,"+tmstp[13], primary_home_phone = tmstp[4:7]+tmstp[13:16], secondary_address="Country\nCity\nStreet,"+tmstp[15], secondary_home_phone=tmstp[3:6] + tmstp[14:17], mobile_phone = tmstp[0:8], work_phone = tmstp[1:9],fax = tmstp[2:10],email1=tmstp[0:5]+"@gmail.com", email2=tmstp[6:10]+"@mail.ru", email3=tmstp[8:11]+"@myjob.com",homepage = "https://"+tmstp[13:20]+".com", birthdate = birthdate, anniversary_date = anniversary_date, group = group, notes="here is my note")
-    app.create_contact(contact)
-    app.logout()
+    app.contact.create(contact)
+    app.session.logout()
 
 
