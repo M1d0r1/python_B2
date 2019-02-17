e20e0409d88d8734b9be936983dae6bed703d8d8
M1d0r1 <ultragreen@gmail.com>
2019-02-17 22:17:22 +0300

    work with webdriver in SessionHelper and ClassHelper has been changed a little

--- fixture/group.py
+++ fixture/group.py
@@ -2,21 +2,21 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
-
     def create(self, group):
         # Init group creation
         self.app.navigate_to_groups()
-        self.app.wd.find_element_by_name("new").click()
+        wd = self.app.wd
+        wd.find_element_by_name("new").click()
         # Fill in the form
-        self.app.wd.find_element_by_name("group_name").click()
-        self.app.wd.find_element_by_name("group_name").clear()
-        self.app.wd.find_element_by_name("group_name").send_keys(group.name)
-        self.app.wd.find_element_by_name("group_header").click()
-        self.app.wd.find_element_by_name("group_header").clear()
-        self.app.wd.find_element_by_name("group_header").send_keys(group.header)
-        self.app.wd.find_element_by_name("group_footer").click()
-        self.app.wd.find_element_by_name("group_footer").clear()
-        self.app.wd.find_element_by_name("group_footer").send_keys(group.footer)
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # Submit group creation and navigate to groups
-        self.app.wd.find_element_by_name("submit").click()
+        wd.find_element_by_name("submit").click()
         self.app.navigate_to_groups()
--- fixture/session.py
+++ fixture/session.py
@@ -4,11 +4,12 @@ class SessionHelper:
 
     def login(self, admin, password):
         self.app.open_start_page()
-        self.app.wd.find_element_by_name("user").clear()
-        self.app.wd.find_element_by_name("user").send_keys(admin)
-        self.app.wd.find_element_by_name("pass").clear()
-        self.app.wd.find_element_by_name("pass").send_keys(password)
-        self.app.wd.find_element_by_xpath("//input[@value='Login']").click()
+        wd = self.app.wd
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(admin)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def logout(self):
         self.app.wd.find_element_by_link_text("Logout").click()
