ab03cfaed63ce3aeb671bfd5e0678239eff91a39
M1d0r1 <ultragreen@gmail.com>
2019-02-19 22:18:22 +0300

    test_modify_group was added. Simple test with simple attributes.

--- fixture/group.py
+++ fixture/group.py
@@ -27,3 +27,26 @@ class GroupHelper:
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("delete").click()
         self.app.navigate_to_groups()
+
+    def modify_first(self):
+        self.app.navigate_to_groups()
+        # Init group modification
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("edit").click()
+        # Update group parameters
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("Upd")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("Upd")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("Upd")
+        # Submit group creation and navigate to groups
+        wd.find_element_by_name("update").click()
+        self.app.navigate_to_groups()
+
+
+        self.app.navigate_to_groups()
