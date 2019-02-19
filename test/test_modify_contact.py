07fe75d9bbfce99edfbea8fd4aed69077660b6dd
M1d0r1 <ultragreen@gmail.com>
2019-02-19 23:09:06 +0300

    test_modify_contact was created. Dates are not filled in yet.

---
+++ test/test_modify_contact.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_modify_first_contact(app):
+    app.session.login(admin="admin", password="secret")
+    app.contact.modify_first()
+    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,5 +1,4 @@
 def test_modify_first_group(app):
     app.session.login(admin="admin", password="secret")
-    # Set the index in order to distinguish the created groups
     app.group.modify_first()
     app.session.logout()
