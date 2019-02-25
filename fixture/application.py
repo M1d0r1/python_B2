111ae22fdd7e90c45622aa4b5455e865b849a081
M1d0r1 <ultragreen@gmail.com>
2019-02-25 15:49:33 +0300

    implicitly_wait was set to 2

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import Select
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        #self.wd.implicitly_wait(5)
+        self.wd.implicitly_wait(2)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
