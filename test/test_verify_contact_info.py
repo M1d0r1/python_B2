c6a3d75c76ed8bbe41f9e5041ca4a54efc379bc2
M1d0r1 <ultragreen@gmail.com>
2019-03-08 22:38:38 +0300

    test_verify_info works only for names, address, emails and phones in home page

--- test/test_verify_contact_info.py
+++ test/test_verify_contact_info.py
@@ -12,6 +12,7 @@ def test_verify_info_on_home_page(app):
     assert contact_from_edit_page.last_name == contact_from_home_page.last_name
     assert contact_from_edit_page.primary_address == contact_from_home_page.primary_address
     assert contact_from_home_page.all_phones == merge_phones_like_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_emails == merge_emails_like_home_page(contact_from_edit_page)
     #assert contact_from_edit_page.email1 == contact_from_home_page.email1
     #assert contact_from_edit_page.email2 == contact_from_home_page.email2
     #assert contact_from_edit_page.email3 == contact_from_home_page.email3
@@ -21,6 +22,10 @@ def merge_phones_like_home_page(contact):
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.primary_home_phone, contact.mobile_phone, contact.work_phone, contact.secondary_home_phone]))))
+
+def merge_emails_like_home_page(contact):
+    return "\n".join(filter(lambda x: x is not None,[contact.email1, contact.email2, contact.email3]))
+
 
 def clear(s):
     return re.sub("[- )(]","",s)
