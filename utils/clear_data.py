7a865ec8320d013868284f2412a276bf2efdb954
M1d0r1 <ultragreen@gmail.com>
2019-03-13 18:34:23 +0300

    Utils for data clearing

---
+++ utils/clear_data.py
@@ -0,0 +1,12 @@
+def test_clear_groups(app):
+    for i in range(0,app.group.count()):
+         app.group.delete_by_index(0)
+
+
+def test_clear_contacts(app):
+    for i in range(0,app.contact.count()):
+         app.contact.delete_by_index(0)
+         app.open_start_page()
+         app.wd.find_element_by_link_text("add new")
+
+
