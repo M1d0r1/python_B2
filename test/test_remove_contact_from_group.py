d8e0e0b38ecc9f5af6a5810f5b9c6525e965f9cb
M1d0r1 <ultragreen@gmail.com>
2019-04-11 09:24:41 +0300

    test_remove_contact_from_group is working when preconditions are not ready - update

--- test/test_remove_contact_from_group.py
+++ test/test_remove_contact_from_group.py
@@ -4,25 +4,23 @@ from model.group import Group
 
 
 def test_remove_contact_from_group(app, db, orm, check_ui):
-    print("GROUPS: ", orm.get_group_list(), len(orm.get_group_list()))
-    print("EMPTY GROUPS: ", orm.get_empty_group_list(), len(orm.get_empty_group_list()))
-    print("NOT EMPTY GROUPS: ", orm.get_not_empty_group_list(), len(orm.get_not_empty_group_list()))
-    if len(orm.get_group_list()) == len(orm.get_empty_group_list()):
+    if app.group.count() == 0:
         new_group = Group(name="Group for removing contacts")
         app.group.create(new_group)
-        sorted_groups = sorted(orm.get_group_list(), key = Group.id_or_max)
-        new_group.id = sorted_groups[-1].id
-        new_contact = Contact(first_name = "Contact for removing")
+    if app.contact.count() == 0:
+        new_contact = Contact(first_name="Contact for removing")
         app.contact.create(new_contact)
-        sorted_contacts = sorted(orm.get_contact_list(), key=Contact.id_or_max)
-        new_contact.id = sorted_contacts[-1].id
-        app.contact.add_to_group(new_contact, new_group)
     groups = orm.get_not_empty_group_list()
+    if len(groups)== 0:
+        group = random.choice(orm.get_group_list())
+        contact = random.choice(orm.get_contacts_not_in_group(group))
+        app.contact.add_to_group(contact, group)
+        groups = orm.get_not_empty_group_list()
     group = random.choice(groups)
     old_contacts_in_group = orm.get_contacts_in_group(group)
-    contact= random.choice(old_contacts_in_group)
-    app.contact.remove_from_group(contact, group)
-    old_contacts_in_group.remove(contact)
+    contact_to_remove = random.choice(old_contacts_in_group)
+    app.contact.remove_from_group(contact_to_remove, group)
+    old_contacts_in_group.remove(contact_to_remove)
     new_contacts_in_group = orm.get_contacts_in_group(group)
     assert sorted(old_contacts_in_group, key = Contact.id_or_max) == sorted(new_contacts_in_group, key = Contact.id_or_max)
 
