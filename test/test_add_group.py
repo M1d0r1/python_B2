d557b3143c7c5878fd19f50f084446418d5ad5ce
M1d0r1 <ultragreen@gmail.com>
2019-04-18 23:50:52 +0300

    allure-report was generated

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
+import allure
 
 def test_add_group(app, db, check_ui, json_groups):
     new_group = json_groups
-    with pytest.allure.step("Given a group list"):
+    with allure.step("Get old groups list"):
          old_groups = db.get_group_list()
-    with pytest.allure.step("When I add a group %s to the list" % new_group):
+    with allure.step("When I add a group %s to the list" % new_group):
         app.group.create(new_group)
-    with pytest.allure.step("Then the new list of groups is equal to the old list with the added group"):
+    with allure.step("Then the new list of groups is equal to the old list with the added group"):
         new_groups = db.get_group_list()
         old_groups.append(new_group.clear())
         assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
